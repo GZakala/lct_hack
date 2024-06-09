7a4f67663bd5a43eb40f2fc6d35b946a31d93284
George Zakala <georgiy.zakala@gmail.com>
2024-06-09 21:43:23 +0300

    Обновил телеграм бот

--- telegram_bot/app.py
+++ telegram_bot/app.py
@@ -1,13 +1,59 @@
+import sys
+from pathlib import Path
+sys.path.insert(0, str(Path(__file__).parent.absolute()))
+
+
 import json
 import requests
+from functools import wraps
 
 import telebot
+from flask import Flask, request
+from keycloak import KeycloakOpenID
+
+from config import Config
+
+cfg = Config.load()
+app = Flask(__name__)
+bot = telebot.TeleBot(cfg.api_token)
+keycloak_openid = KeycloakOpenID(
+	server_url=cfg.keycloak_server_url,
+    client_id=cfg.keycloak_client_id,
+    realm_name=cfg.keycloak_realm_name,
+    client_secret_key=cfg.keycloak_client_secret_key,
+)
 
 
-API_TOKEN = '7292865506:AAHk1-6hUxhaDCEYg9lbXWG3RwBcf2RwxWs'
-bot = telebot.TeleBot(API_TOKEN)
+@app.route('/callback')
+def callback():
+	global access_token
+	authorization_code = request.args.get('code')
+	if not authorization_code:
+		return "Authorization code not provided", 400
 
-@bot.message_handler(commands=['start', 'info'])
+	token_response = keycloak_openid.token(authorization_code, cfg.keycloak_redirect_url)
+	access_token = token_response['access_token']
+	refresh_token = token_response['refresh_token']
+	
+	return f"Access Token: {access_token}<br>Refresh Token: {refresh_token}"
+
+def requires_auth(func):
+    @wraps(func)
+    def wrapper(message, *args, **kwargs):
+        global access_token
+        if not access_token:
+            bot.reply_to(message, "�?�? �?�? ���?�?�?���?���?�?. �?�?�?��?�?�?�?�?�, ��?��?�?�?��?�? ���?�?�?����?��? �?�?�?�?� /start")
+            return
+        return func(message, *args, **kwargs)
+    return wrapper
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    auth_url = keycloak_openid.auth_url(redirect_uri=cfg.keycloak_redirect_uri)
+    bot.reply_to(message, f"�?�?�?�?�?���?�? ��? �?�?�?�?��? ��?�? ���?�?�?����?��: {auth_url}")
+
+@bot.message_handler(commands=['info'])
+@requires_auth
 def send_welcome(message: telebot.types.Message):
 	bot.reply_to(
 		message, 
@@ -21,6 +67,7 @@ def send_welcome(message: telebot.types.Message):
 	)
 
 @bot.message_handler(commands=['search_ste'])
+@requires_auth
 def search_ste(message: telebot.types.Message):
 	response = requests.get(
 		url='http://hack_statistics_server:5000/search_ste', 
@@ -33,7 +80,7 @@ def search_ste(message: telebot.types.Message):
 		return bot.reply_to(message, 'Я не нашел КПГЗ по вашему запросу')
 	for row in rows:
 		response_text += f"""
-СТЕ: {row.get('ste_name', '')}
+СТЕ?: {row.get('ste_name', '')}
 Характеристика СТЕ: {row.get('ste_name', '')}
 		"""
 		if row['kpgz_code'] != '':
@@ -44,6 +91,7 @@ def search_ste(message: telebot.types.Message):
 	bot.reply_to(message, response_text)
 
 @bot.message_handler(commands=['search_kpgz'])
+@requires_auth
 def search_kpgz(message: telebot.types.Message):
 	response = requests.get(
 		url='http://hack_statistics_server:5000/search_kpgz', 
@@ -53,10 +101,10 @@ def search_kpgz(message: telebot.types.Message):
 	response_text = 'Я нешел следующие КПГЗ по вашему запросу\n'
 	rows = json.loads(response.content)
 	if not rows:
-		return bot.reply_to(message, 'Я не нашел КПГЗ по вашему запросу')
+		return bot.reply_to(message, '')
 	for row in rows:
 		response_text += "\n"
-		if row['kpgz_code'] != '':
+		if row['kpgz_code'] != 'Я не нашел КПГЗ по вашему запросу':
 			response_text += f"КПГЗ: {row['kpgz_code']} - {row['kpgz_name']}\n"
 		if row['spgz_code'] != '':
 			response_text += f"СПГЗ: {row['spgz_code']} - {row['spgz_name']}\n"
@@ -65,4 +113,6 @@ def search_kpgz(message: telebot.types.Message):
 
 
 if __name__ == '__main__':
+	from threading import Thread
+	Thread(target=app.run, kwargs={'host':'0.0.0.0', 'port': 8000}).start()
 	bot.infinity_polling()
---
+++ telegram_bot/config.py
@@ -0,0 +1,29 @@
+import os
+
+class Config:
+	def __init__(
+		self,
+		keycloak_server_url: str,
+		keycloak_realm_name: str,
+		keycloak_client_id: str,
+		keycloak_client_secret: str,
+		keycloak_redirect_url: str,
+		api_token: str,
+	):
+		self.keycloak_server_url = keycloak_server_url
+		self.keycloak_realm_name = keycloak_realm_name
+		self.keycloak_client_id = keycloak_client_id
+		self.keycloak_client_secret = keycloak_client_secret
+		self.keycloak_redirect_url = keycloak_redirect_url
+		self.api_token = api_token
+
+	@classmethod
+	def load(cls):
+		return cls(
+			keycloak_server_url=os.getenv("KEYCLOAK_SERVER_URL"),
+			keycloak_realm_name=os.getenv("KEYCLOAK_REALM_NAME"),
+			keycloak_client_id=os.getenv("KEYCLOAK_CLIENT_ID"),
+			keycloak_client_secret=os.getenv("KEYCLOAK_CLIENT_SECRET"),
+			keycloak_redirect_url=os.getenv("KEYCLOAK_REDIRECT_URI"),
+			api_token=os.getenv("API_TOKEN"),
+		)
